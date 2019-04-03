e48ddf46e8cb0ac69dd882432eca46fa08ff6da2
ChekeGT <chekelosos@gmail.com>
2019-04-03 16:34:54 -0600

    Finally, created the main interphace.

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+"""Manages all the graphicals represntations."""
+
 # PyQt
 from PyQt5.QtGui import QIcon, QPalette, QColor, QPixmap, QFont
 from PyQt5.QtCore import Qt
@@ -10,9 +12,10 @@ from PyQt5.QtWidgets import (
 	QLineEdit,
 	QPushButton,
     QMessageBox,
-    QDialog
+    QDialog,
+    QCheckBox,
+    QFileDialog
 )
-from PyQt5 import uic
 
 # Authentication
 from authentication import Authentication
@@ -20,14 +23,259 @@ from authentication import Authentication
 # Sys
 import sys
 
+# Cleaners
+from cleaners import M3UFileCleaner
+
+# Readers
+from readers import M3UFileReader
+
+# Utilities
+from utilites import ParseAndWriteM3UToSimpleText
+
 
 class ParsingCleaningWindow(QDialog):
+    """Manages the graphical representation of changing iptv lists."""
 
     def __init__(self, parent=None):
+        """Handles initialization of the class."""
         super(ParsingCleaningWindow, self).__init__(parent)
+
         self.setWindowTitle('Iptv File Cleaner | Limpiar o cambiar formato')
         self.setWindowIcon(QIcon("img/icon.png"))
-        uic.loadUi('ui/parsing_cleaning.ui')
+
+        self.setFixedSize(450, 450)
+        self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.MSWindowsFixedSizeDialogHint)
+
+        self.patterns = [
+        ]
+
+        self.input_file = None
+        self.output_file = None
+
+        self.initUi()
+
+    def initUi(self):
+        """Handles the initialization of all the graphical interphace"""
+
+        # Headers
+
+        palette = QPalette()
+        palette.setColor(QPalette.Background, QColor(51, 0, 102))
+
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.NoFrame)
+        frame.setFrameShadow(QFrame.Sunken)
+        frame.setAutoFillBackground(True)
+        frame.setPalette(palette)
+        frame.setFixedWidth(450)
+        frame.setFixedHeight(84)
+        frame.move(0, 0)
+
+        iconLabel = QLabel(frame)
+        iconLabel.setFixedWidth(40)
+        iconLabel.setFixedHeight(40)
+        iconLabel.setPixmap(QPixmap("img/icon.png").scaled(40, 40, Qt.KeepAspectRatio,
+                                                         Qt.SmoothTransformation))
+        iconLabel.move(37, 22)
+
+        titleFont = QFont()
+        titleFont.setPointSize(16)
+        titleFont.setBold(True)
+
+        titleLabel = QLabel("<font color='white'>Iptv File Cleaner.</font>", frame)
+        titleLabel.setFont(titleFont)
+        titleLabel.move(83, 20)
+
+        subtitleFont = QFont()
+        subtitleFont.setPointSize(9)
+
+        subtitleLabel = QLabel("<font color='white'>Limpia o cambia de formato tu lista de iptv."
+                                "</font>", frame)
+        subtitleLabel.setFont(subtitleFont)
+        subtitleLabel.move(111, 46)
+        
+        # Input File
+
+        # Label
+
+        inputFileLabel = QLabel("Selecciona tu lista:", self)
+        inputFileLabel.move(40, 95)
+
+        # Button
+
+        inputFileSelectorButton = QPushButton("Seleccionar Archivo:", self)
+        inputFileSelectorButton.setFixedWidth(170)
+        inputFileSelectorButton.setFixedHeight(30)
+        inputFileSelectorButton.move(180, 90)
+
+        # Patterns
+
+        # Common Patterns
+
+        # Series
+
+        deleteSeriesLabel = QLabel("Eliminar Series?", self)
+        deleteSeriesLabel.move(60, 150)
+
+        self.deleteSeries = QCheckBox(self)
+        self.deleteSeries.move(40, 150)
+
+        # Movies
+
+        deleteMoviesLabel = QLabel("Eliminar Peliculas?", self)
+        deleteMoviesLabel.move(60, 170)
+
+        self.deleteMovies = QCheckBox(self)
+        self.deleteMovies.move(40, 170)
+
+        # Custom patterns
+
+        patternsLabel = QLabel("Introduce un patron, y da ok para agregarlo a la lista.", self)
+        patternsLabel.move(40, 210)
+
+        patternsFrame = QFrame(self)
+        patternsFrame.setFrameShape(QFrame.StyledPanel)
+        patternsFrame.setFixedWidth(280)
+        patternsFrame.setFixedHeight(28)
+        patternsFrame.move(40, 236)
+
+        self.patternLineEdit = QLineEdit(patternsFrame)
+        self.patternLineEdit.setFrame(False)
+        self.patternLineEdit.setTextMargins(8, 0, 4, 1)
+        self.patternLineEdit.setFixedWidth(238)
+        self.patternLineEdit.setFixedHeight(26)
+        self.patternLineEdit.move(40, 1)
+
+        # Image
+        patternsImage = QLabel(patternsFrame)
+        patternsImage.setPixmap(QPixmap("img/book.jpg").scaled(
+                30,
+                30, 
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation
+            )
+        )
+        patternsImage.move(10, 4)
+
+        # Confirm pattern button
+
+        confirmPatternButton = QPushButton("Ok", self)
+        confirmPatternButton.setFixedWidth(50)
+        confirmPatternButton.setFixedHeight(30)
+        confirmPatternButton.move(330, 240)
+
+        # Decoder Type
+
+        decoderTypeLabel = QLabel("Tipo de decodificador", self)
+        decoderTypeLabel.move(40, 290)
+
+        self.decoderType = QComboBox(self)
+        self.decoderType.addItems(["OpenBox", "Freesat | GT Media"])
+        self.decoderType.setCurrentIndex(-1)
+        self.decoderType.setFixedWidth(280)
+        self.decoderType.setFixedHeight(26)
+        self.decoderType.move(40, 306)
+
+        # Output File
+
+        # Label
+
+        outputFileLabel = QLabel("Archivo de Salida:", self)
+        outputFileLabel.move(40, 360)
+
+        # Button
+
+        outputFileSelectorButton = QPushButton("Seleccionar Archivo:", self)
+        outputFileSelectorButton.setFixedWidth(170)
+        outputFileSelectorButton.setFixedHeight(30)
+        outputFileSelectorButton.move(180, 355)
+
+        # Change list button
+
+        changeIptvListButton = QPushButton("Limpiar y/o cambiar lista.", self)
+        changeIptvListButton.setFixedWidth(300)
+        changeIptvListButton.setFixedHeight(30)
+        changeIptvListButton.move(70, 415)
+
+        # Buttons Signals
+
+        confirmPatternButton.clicked.connect(self.add_pattern)
+        changeIptvListButton.clicked.connect(self.changeIptvList)
+        inputFileSelectorButton.clicked.connect(self.get_input_file)
+        outputFileSelectorButton.clicked.connect(self.get_output_file)
+
+    def add_pattern(self):
+        """Handles adding patterns to the patterns list."""
+
+        pattern = self.patternLineEdit.text()
+        self.patterns.append(pattern)
+
+        self.patternLineEdit.clear()
+
+    def get_input_file(self):
+        filename, file_type = QFileDialog.getOpenFileName(self)
+        
+        file = open(filename, 'r+', encoding='utf-8')
+
+        self.input_file = file
+
+    def get_output_file(self):
+        filename, file_type = QFileDialog.getOpenFileName(self)
+        
+        file = open(filename, 'w', encoding='utf-8')
+
+        self.output_file = file
+
+    def changeIptvList(self):
+        """Handles the parsing or/and cleaning of an IptvList"""
+
+        input_file = self.input_file
+        output_file = self.output_file
+        msgBox = QMessageBox()
+
+        if input_file and output_file:
+
+            patterns = self.patterns
+
+            if self.deleteMovies.isChecked():
+                patterns += [
+                    'movie',
+                    'movies',
+                    'peliculas',
+                    'pelicula'
+                ]
+
+            if self.deleteSeries.isChecked():
+                patterns += [
+                    'serie',
+                    'series'
+                ]
+
+            decoder_type = self.decoderType.currentText()
+            if decoder_type:
+
+                if len(patterns) >= 1:
+
+                    cleaner = M3UFileCleaner(patterns)
+                    cleaned_lines = cleaner.delete_unneeded_lines(input_file)
+
+                    segment_list = M3UFileReader.cut_lines(cleaned_lines)
+
+                else:
+                    segment_list, length = M3UFileReader.read(input_file)
+                
+                ParseAndWriteM3UToSimpleText(input_file, output_file, decoder_type, segment_list)
+
+                msgBox.setWindowTitle('Lineas borradas y cambiadas exitosamente.')
+                msgBox.setText('Todas las lineas han sido cambiadas de formato y limpiadas para adaptarse a tu deco ;).')
+            else:
+                msgBox.setWindowTitle('Error')
+                msgBox.setText('Necesitas poner un decodificador al cual pasar el archivo.')
+        else:
+            msgBox.setWindowTitle('Error')
+            msgBox.setText('No has seleccionado ningun archivo, seleccionalos porfavor.')
+
+        msgBox.exec_()
 
 
 class WindowLogin(QMainWindow):
@@ -53,6 +301,8 @@ class WindowLogin(QMainWindow):
     def initUI(self):
         """Sets up the graphical interphace."""
 
+        # Headers
+        
         palette = QPalette()
         palette.setColor(QPalette.Background, QColor(51, 0, 102))
 
@@ -88,6 +338,7 @@ class WindowLogin(QMainWindow):
         subtitleLabel.setFont(subtitleFont)
         subtitleLabel.move(111, 46)
 
+        # User 
 
         userLabel = QLabel("Usuario", self)
         userLabel.move(60, 170)
@@ -110,7 +361,8 @@ class WindowLogin(QMainWindow):
         self.lineEditUser.setFixedHeight(26)
         self.lineEditUser.move(40, 1)
 
-
+        # Password
+        
         passwordLabel = QLabel("Contraseña", self)
         passwordLabel.move(60, 224)
 
@@ -133,6 +385,7 @@ class WindowLogin(QMainWindow):
         self.lineEditPassword.setFixedHeight(26)
         self.lineEditPassword.move(40, 1)
 
+        # Buttons
 
         loginButton = QPushButton("Iniciar sesión", self)
         loginButton.setFixedWidth(135)
@@ -144,6 +397,8 @@ class WindowLogin(QMainWindow):
         cancelButton.setFixedHeight(28)
         cancelButton.move(205, 286)
 
+        # Information
+
         infoLink = 'https://localhost:63342/index.html'
 
         informationLabel = QLabel(f"<a href='{infoLink}'>Comprar</a>.", self)
@@ -151,6 +406,7 @@ class WindowLogin(QMainWindow):
         informationLabel.setToolTip("Create una cuenta y compra el programa")
         informationLabel.move(15, 344)
 
+        # Buttons Signals
 
         loginButton.clicked.connect(self.login)
         cancelButton.clicked.connect(self.close)
@@ -188,7 +444,7 @@ class WindowLogin(QMainWindow):
         """Handles opening the window that contains all the logic of parsing and cleaning lists."""
 
         self.close()
-        self.parsing_cleaning_window.exec_()
+        self.parsing_cleaning_window.show()
 
 
 if __name__ == '__main__':
--- utilites.py
+++ utilites.py
@@ -1,9 +1,20 @@
-# Utilities
-from os import system, name
+"""All the stuff that i dont know how to name ends here."""
 
+# Parsers
+from parsers import SimpleTextParser
 
-def cls():
-	if name == 'posix':
-		system("clear")
-	else:
-		system("cls")
+# Writers
+from writers import BaseFileWriter
+
+def ParseAndWriteM3UToSimpleText(output_file, decoder_type, segment_list):
+	"""Manages parsing and writing depending on the decoder_type"""
+
+	if decoder_type == 'OpenBox':
+		OpenBoxParser = SimpleTextParser.M3U(",")
+		parsed_lines = OpenBoxParser.parse(segment_list)
+
+	elif decoder_type == 'Freesat | GT Media':
+		FreesatGtMediaparser = SimpleTextParser.M3U(" ")
+		parsed_lines = FreesatGtMediaparser.parse(segment_list)
+
+	BaseFileWriter.write_lines(parsed_lines, output_file)
